c0988118f7d31528e6fb359ccaf1221958031034
Mike Bearman <29874229+bearmannl@users.noreply.github.com>
2025-09-16 10:11:44 +0200

    Add VHD image generation note to config on sizing.

--- generators/azure/developer-vm/configuration.nix
+++ generators/azure/developer-vm/configuration.nix
@@ -4,6 +4,9 @@
   # Use the import to add even more split modules into the config
   imports = [ ];
   # set disk size to to 128G | As per Standard SSD E10
+  # Note! When generating VHD files, it gets allocated to memory before
+  # dumping into a file. If you have 32GB of RAM (incl. possible swap),
+  # you will get an out of memory error when generating the VHD image.
   virtualisation = { diskSize = 128 * 1024; };
   # Set a generic started networking hostname.
   networking = { hostName = "nixos-azure-developer-x"; };
